d951dc91082cb55337b58396a0c600877386c7f2
Rupesh Khode <khoderupesh@gmail.com>
2024-01-13 20:29:06 +0530

    added toggling dark light mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 // import './App.css';
-import About from './components/about';
+// import About from './components/about';
 import Navbar from './components/navbar';
 import TextForms from './components/textforms';
+import React, { useState } from 'react'
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const toggleMode = () => {
+    if(mode === 'light') {
+      setMode('dark')
+    } else {
+      setMode('light');
+    }
+  }
   return (
     <>
-    <Navbar title="TextUtils"  />
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
     <div className="container">
       <TextForms heading="Text Casing Converter App"/>
       {/* <About/> */}
