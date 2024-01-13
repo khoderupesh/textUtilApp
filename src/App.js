24e0fce5ee19809851a68da10d89e09258095d43
Rupesh Khode <khoderupesh@gmail.com>
2024-01-13 17:19:48 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import './App.css';
+import Navbar from './components/navbar';
+import TextForms from './components/textforms';
+
+function App() {
+  return (
+    <>
+    <Navbar title="TextUtils"  />
+    <TextForms heading="Text Casing Converter App"/>
+    </>
+  );
+}
+
+export default App;
